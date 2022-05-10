b5e7ec4ad934b67fe4c33e58ac582402c56ce1c4
klooopmoo <gc.franco93@gmail.com>
2022-05-10 15:59:52 -0700

    database now works with config rather than hard coding. Also added zerologging with debugging as true in .env file

--- backend/main.go
+++ backend/main.go
@@ -5,14 +5,28 @@ import (
 	"github.com/SAKA-club/todo/backend/gen/restapi/operations"
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/runtime"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
+var host = "None"
+
 func main() {
+	// Get configs
+	cfg := LoadConfig()
+	initLog(cfg)
+
 	//swagger
 	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to load swagger spec")
+	}
+
+	//local debug
+	if cfg.LocalDebug {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		log.Error().Msg("debug mode")
 	}
 
 	api := operations.NewTodoAPI(swaggerSpec)
@@ -28,11 +42,24 @@ func main() {
 		}
 	}(server)
 
-	server.Port = 8080
-	server.Host = "0.0.0.0"
+	server.Port = cfg.Port
+	server.Host = cfg.Host
 
 	if err := server.Serve(); err != nil {
 		log.Panic().Err(err).Msg("server error")
 	}
 
+}
+
+func initLog(cfg *Config) {
+	var err error
+
+	host, err = os.Hostname()
+	if err != nil {
+		log.Panic().Err(err).Msg("unable to get hostname")
+	}
+
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	log.Log().Str("Host", host).Msg("Service Startup")
 }
